240c77542e2f6f038716cc9768cb28d67c258c49
Kami <theonekamisama@gmail.com>
2019-02-14 23:05:25 +0530

    Update Qualie.py

--- Qualie.py
+++ Qualie.py
@@ -10,8 +10,6 @@ import random
 import math
 import requests
 
-bot_admin_discriminators = [256390874848690176,131205596732063744] # These users have access to the bot's admin functions on all servers
-
 def value_in_list(ls, val): # ls: a list; val: a value
     for test_value in ls:
         if (test_value == val):
@@ -42,33 +40,26 @@ def basic_check(ctx):  ##for funsies
 
 home=None 
 
-client=commands.Bot( command_prefix=('?', '!','.', 'cc ', 'Cc ','CC ', 'Coffee ','Coffee Cat '),description='Alright a little something i did for both expertimentaion and Hapiness. This is Yuno')
-client.remove_command('help')
+client=commands.Bot( command_prefix=('?', '!','.', 'q ', 'Q ','Qualie '))
+
 
 
 @client.event
 async def on_ready():
-    print('You are running BasicBot v2.1')
+    print('Quailie ready')
     print('Created by Kaminolucky')
-    client.load_extension("Role_command")
-    client.load_extension("Magic")
-    client.load_extension("Net_command")
-    client.load_extension("Sl_command")
-    client.load_extension("doku")
-    client.load_extension("sentience")
-    client.load_extension("test")
 ##
     home=client.get_channel(id=522127036022521871)
 
-    await home.send("I am REBORN")
+    await home.send("Quailie reporting in")
 
-    return await client.change_presence(activity=discord.Game(name='Am i Pretty yet?'))
+    return await client.change_presence(activity=discord.Game(name='My status?'))
 
 
 @client.event
 async def on_member_join(member):
     x = client.get_channel(id=377790511353692162)
-    y="Hello" + member.mention+"I am Coffee Cat and it’s a pleasure to meet you. Welcome to this relaxed community server. Please read #rules-faq to get started and use !help for my commands."
+    y="Hello" + member.mention
     em = discord.Embed(title="New Face")
     em.set_image(url=member.avatar_url)
     await x.send(y)
@@ -77,7 +68,7 @@ async def on_member_join(member):
             
 @client.command()
 async def hi(ctx):
-    await ctx.send("Cest La Vie")
+    await ctx.send("generic hi message")
 
 @client.command()
 async def pick(ctx, *, args):
@@ -112,52 +103,7 @@ async def roll(ctx, *, args):
     await ctx.send(res)
 
 
-@client.command()
-async def ask(ctx, *,args=None):
-    if(args==None):
-        await ctx.send("The answer is. FUCK you for not asking a question.")
-        await ctx.send("Was that rude?")
-        return
-    args=args.lower()
-    x=""
-    if ("kms" in args or "kill" in args):
-        x+="Killing is bad, yet... "
-    y=["Sure",
-       "It checks out",
-       "Without a doubt",
-       "Yes - definitely",
-       "T R U S T",
-       "i can't see the future, but it's a yes",
-       "probabilty says..something, But you should TAKE THE CHANCE" ,
-       "Looks fair",
-       "Yes.",
-       "Sure.",
-       "I'd rather not answer it.",
-       "Ask again later.",
-       "Better not tell you now.",
-       "Cannot predict now.",
-       "50/50...wait no 49/51",
-       "50/50...wait no 51/49",
-       "No",
-       "Why tf?",
-       "I wouldn't do that",
-       "I have run many calcs, they all say no....So..."]
-    x+= random.choice(y)
-    await ctx.send(x)
 
-
-
-@client.command()
-async def dab(ctx, *, args='1'):
-    y = int(args)
-    t=0
-    if y > 14 and not(basic_check(ctx)):
-        y = 14
-    dab=["<:dab:407026257969152031>","<:pandab:524980250170490892>"]
-    for i in range(y):
-        await ctx.send(dab[t])
-        t=not(t)
-        await asyncio.sleep(0.5)
 
 @client.command() 
 async def calc(ctx, *, args):  
@@ -167,27 +113,6 @@ async def calc(ctx, *, args):
     except ZeroDivisionError :  
         x = 'Bish , you just divided by zero'  
     await ctx.send('Result: ' + str(x))
-
-
-@client.command()
-@commands.check(basic_check)
-async def rproom(ctx):
-    y=None
-    for i in ctx.guild.categories:
-        if( i.id==377792065192460289):
-            y=i
-    await ctx.guild.create_text_channel("room_2",category=y)
-    await ctx.send("Channel created. Have fun")
-
-@client.command()
-@commands.check(basic_check)
-async def deleterproom(ctx):
-    y=None
-    for i in ctx.guild.text_channels:
-        if( i.name=="room_2"):
-            y=i
-    await y.delete()
-    await ctx.author.send("Channel Killed!")
 
 
 ###
@@ -218,28 +143,6 @@ async def time(ctx):
 
     await ctx.send(embed=em)
 
-##
-## WHAT IS THIS TRASH HELP
-## R E E E E
-##
-
-@client.command()
-async def help(ctx):
-    x= discord.Embed(title= "HELP")
-    x.add_field(name="Help",value="Syntax: !help \nUse: Displays this message", inline=False)
-    x.add_field(name="Hi",value="Syntax: !hi \nUse: To test if the bot is on or not", inline=False)
-    x.add_field(name="Pick",value="Syntax: !pick choice a, choice b....,choice n \nUse: To pick out of the given choices", inline=False)
-    x.add_field(name="Roll",value="Syntax: !roll <no of dice>d<no of sides> \nUse: To roll dice. \nEg !roll 1d20", inline=False)
-    x.add_field(name="Dab",value="Syntax: !dab \nUse: GUESS YOU BLOODY BISHES", inline=False)
-    x.add_field(name="Cat",value="Syntax: !cat \nUse: Cat Pix ^-^", inline=False)
-    x.add_field(name="Dog",value="Syntax: !dog \nUse: DOGGO", inline=False)
-    x.add_field(name="Docs",value="Syntax:!docs \nUse: Shows the docs of all the rps in this server", inline=False)
-    x.add_field(name="Starless",value="Syntax: !sl \nUse: Shows docs relavent to sl", inline=False)
-    x.add_field(name="Calc",value="Syntax: !calc <expresion> \nUse: Calculates yoru expression, us +, -, *, / \n Eg !calc 3+2*3", inline=False)
-    x.add_field(name="Time",value="Syntax: !time \nUse: tells time in diffrent regions, if you region is not there pm kami", inline=False)
-    x.add_field(name="role",value="Syntax: !<> \nUse: Assigns the said role. use !rolename to geet the role", inline=False)
-    await ctx.send(embed=x)
-
 
 
 @client.command()
@@ -248,12 +151,6 @@ async def in_guilds(ctx):
         await ctx.send(i.name)
 
 
-@client.command()
-@commands.check(Kami_check)
-async def test(ctx):
-    m=client.get_channel(id="528254975726583818")
-    m.send("Placeholder Message of the day")
-    await ctx.send("ok")
 
 @client.command()
 async def rtfm(ctx):
@@ -274,13 +171,7 @@ async def avatar(ctx):
     em.set_image(url=ctx.message.author.avatar_url)
     await ctx.send(embed=em)
 
-@client.command()
-@commands.check(basic_check)
-async def ban(ctx):
-    x= "BeGONe THOT!!!!"
-##    for i in ctx.message.mentions:
-##        await ctx.guild.ban(i)
-    await ctx.send(x)
+
 
 @client.command()
 async def timer(ctx, *, args):
@@ -291,42 +182,8 @@ async def timer(ctx, *, args):
     await asyncio.sleep(float(args)*60)
     await ctx.send("Timer over"+ ctx.message.author.mention) 
 
-@client.command()
-async def reminder(ctx, *, args):
-    args=args.split(',')
-    x=args[1]
-    await ctx.send("Setting timer for " + str(args[0])+ " min(s) to remind you of '" + x + "'")
-    if( not (args[0].isnumeric())):
-        await ctx.send("Stfu and put an actual number u skrub")
-        return
-    await asyncio.sleep(float(args[0])*60)
-    await ctx.send("Timer over"+ ctx.message.author.mention + "\n" + x) 
 
 
-
-@client.command()
-async def cc(ctx, *, args):
-    x="on the wall"
-    a="alpha"
-    args=args.lower()
-    if(x in args and a in args):
-        await ctx.send("Hope is most alpha of them all")
-
-
-    
-
-
-@client.command()
-async def alpha(ctx):
-    await ctx.send("Hope is most alpha of them all")
-
-@client.command()
-async def god(ctx):
-    await ctx.send("Llama is our actual god")
-
-@client.command()
-async def git(ctx):
-    await ctx.send("https://github.com/theonekami/COffee-FInal")
 
 
 
@@ -334,32 +191,4 @@ async def git(ctx):
 client.run(os.environ["TOKEN"])
 
 
-
-##################################################################################
-##                                              NO ENTRY BEYOND THIS POINT
-################################################################################
-
-    
-##@client.command()
-##async def ships(ctx):
-##        x="""
-##    \nsilly mortals and thier fantasies. *throw doc* here
-##
-##    \nhttps://docs.google.com/document/d/1ZRUoxHeY8bKjp0eCOvo6ilu-7O03CwTd2ikRv2iUs9I/edit
-##        """
-##        await ctx.send(x)
-
-##@client.command()
-##async def age(ctx):
-##    x= ctx.guild.created_at
-##    y= datetime.datetime.now()
-##    z=y-x
-##    s= "This server was created at " + x.strftime(("%d %m %y")) + "\nThat makes the age " +str(z.days) + " days"
-##    await ctx.send(s)
-
-
-    
-##@client.command()
-##async def Exit(ctx):
-##    await ctx.guild.leave()
 
