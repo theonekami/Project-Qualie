0f317974ab7f3b276427a629251dc6a2a01ff9c3
Kami <theonekamisama@gmail.com>
2019-02-27 17:04:54 +0530

    Update Qualie.py

--- Qualie.py
+++ Qualie.py
@@ -180,7 +180,7 @@ async def purge(ctx,args):
 async def test(ctx):
     DATABASE_URL = os.environ['DATABASE_URL']
     conn = await asyncpg.connect(DATABASE_URL)
-    x= await conn.execute("""ALTER TABLE users ALTER COLUMN id TYPE numeric""")
+    x= await conn.execute("""drop table users""")
     await ctx.send("works")
     await conn.close()
 
