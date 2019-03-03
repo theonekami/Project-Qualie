75d5c0a8c2076da954455bea3a0c9429cda0f219
theonekami <theonekamisama@gmail.com>
2019-03-03 18:11:39 +0530

    Update Qualie.py

--- Qualie.py
+++ Qualie.py
@@ -180,7 +180,8 @@ async def purge(ctx,args):
 async def test(ctx):
     DATABASE_URL = os.environ['DATABASE_URL']
     conn = await asyncpg.connect(DATABASE_URL)
-    x= await conn.fetch("""UPDATE ITEMS SET STOCK=1 WHERE presence=False""")
+    x= await conn.fetch("""select column_name, data_type, character_maximum_length
+from INFORMATION_SCHEMA.COLUMNS where table_name = 'items'""")
     await ctx.send(x)
     await conn.close()
 
