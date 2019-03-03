e82099273d98f4d6a2edae48d621f8639f587de9
theonekami <theonekamisama@gmail.com>
2019-03-03 07:41:35 +0530

    Update user_commands.py

--- user_commands.py
+++ user_commands.py
@@ -25,14 +25,23 @@ class User_Command(commands.Cog):
         y=await conn.fetch("SELECT * FROM users WHERE id="+str(ctx.message.author.id))
         if(len(y) == 0):
             await ctx.send("creating new user")
-            await conn.execute("INSERT INTO USERS (id, money, smithing, sxtraction) VALUES('" + str(ctx.message.author.id)+ "',0,1,1)")
+            await conn.execute("INSERT INTO USERS (id, money,items, smithing, sxtraction) VALUES('" + str(ctx.message.author.id)+ "'None,0,1,1)")
             y=await conn.fetch("SELECT * FROM users WHERE id="+str(ctx.message.author.id))
         else:
             await ctx.send("Fetching for ")
         await conn.close()
         x= discord.Embed(title= "Info!")
+
         for i in y:
-             x.add_field(name=":gem:"+str(i[2])+ " "+ str(i[0]),value=str(i[1]), inline=False)
+            t=self.bot.get_user(id=i[0])
+            x.add_field(name="Money" ,value="gem"+str(i[1]), inline=True)
+            x.add_field(name="Smithing level", value=str(i[3]),inline=True)
+            x.add_field(name="Extraction level", value=str(i[4]),inline=True)
+            w=""
+            for j in i[2]:
+                w+=j+"\n"
+            x.add_field(name="Items:", value=w,inline=False)\
+            
         await ctx.send(embed=x)
 
     @commands.group()
@@ -43,6 +52,7 @@ class User_Command(commands.Cog):
     async def money_add(self,ctx,args):
         DATABASE_URL = os.environ['DATABASE_URL']
         conn = await asyncpg.connect(DATABASE_URL)
+        x=await conn.fetch("SELECT money FROM users WHERE id="+str(ctx.message.mentions[0].id))
         y=await conn.fetch("UPDATE users SET money ="+ str(args)+" WHERE id=" + str(ctx.message.author.id))
         
 
