23c833abb101881ff87a5ccb51a1c2fc76976cfd
theonekami <theonekamisama@gmail.com>
2019-03-21 22:34:29 +0530

    Update user_commands.py

--- user_commands.py
+++ user_commands.py
@@ -67,7 +67,7 @@ class User_Command(commands.Cog):
             else:
                 u=i[2].split("|")
                 for j in u:
-                    w+=j+"\n"
+                    w+=j+"\n\n"
             x.add_field(name="Items:", value=str(w),inline=False)
             
         await ctx.send(embed=x)
